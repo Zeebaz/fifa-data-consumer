47461fac0beab28338fc03cd4dc4ba66887b31df
Zeebaz <jairosebastianrp@gmail.com>
2022-12-12 22:04:16 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+let entry =
+  '<div class="entire-squad_container__3W4Hl"><div class="entire-squad_title__2cx5a">Arquero</div><div class="entire-squad_cardContainer__5coW6 ff-mt-8 ff-mt-lg-16 ff-mb-56"><div class="player-badge-card_badgeCard__2DJ4B"><div class="player-badge-card_playerFlag__rQjVe"><img decoding="async" loading="lazy" height="auto" width="100%" src="https://cloudinary.fifa.com/api/v3/picture/flags-sq-4/ARG?tx=c_fill,g_auto,q_auto" alt="" title="" class="image_img__3ckHZ player-badge-card_flag__2OQ8n"></div><div class="player-badge-card_playerImageContainer__1k0AM"><div class="player-badge-card_playerImage__301X0" style="background-image: url(&quot;https://digitalhub.fifa.com/transform/55e9fe36-b328-44f5-86d8-1cba96a4d05e/1442740056?io=transform:fill,width:792,height:900&quot;);"></div></div><div class="player-badge-card_playerInfo__3Fy6Q ff-mt-8 ff-mt-lg-20"><div class="player-badge-card_playerFirstname__1HlN6"><span class="d-md-none"><span title="Franco" style="display: -webkit-box; -webkit-line-clamp: 1; -webkit-box-orient: vertical; overflow: hidden; word-break: break-all;">Franco</span></span><span class="d-none d-md-block"><span title="Franco" style="display: -webkit-box; -webkit-line-clamp: 1; -webkit-box-orient: vertical; overflow: hidden; word-break: break-all;">Franco</span></span></div><div class="player-badge-card_playerSurname__3_5gf ff-mt-4"><span class="d-md-none"><span title="ARMANI" style="display: -webkit-box; -webkit-line-clamp: 1; -webkit-box-orient: vertical; overflow: hidden; word-break: break-all;">ARMANI</span></span><span class="d-none d-md-block"><span title="ARMANI" style="display: -webkit-box; -webkit-line-clamp: 1; -webkit-box-orient: vertical; overflow: hidden; word-break: break-all;">ARMANI</span></span></div></div><div class="player-badge-card_divider__kBsTp ff-mb-8 ff-mb-lg-16 ff-mt-4 ff-mt-lg-8"></div><div class="player-badge-card_playerPosition__wjnoI"><span class="d-md-none"><span title="Arquero" style="display: -webkit-box; -webkit-line-clamp: 1; -webkit-box-orient: vertical; overflow: hidden; word-break: break-all;">Arquero</span></span><span class="d-none d-md-block"><span title="Arquero" style="display: -webkit-box; -webkit-line-clamp: 1; -webkit-box-orient: vertical; overflow: hidden; word-break: break-all;">Arquero</span></span></div></div><div class="player-badge-card_badgeCard__2DJ4B"><div class="player-badge-card_playerFlag__rQjVe"><img decoding="async" loading="lazy" height="auto" width="100%" src="https://cloudinary.fifa.com/api/v3/picture/flags-sq-4/ARG?tx=c_fill,g_auto,q_auto" alt="" title="" class="image_img__3ckHZ player-badge-card_flag__2OQ8n"></div><div class="player-badge-card_playerImageContainer__1k0AM"><div class="player-badge-card_playerImage__301X0" style="background-image: url(&quot;https://digitalhub.fifa.com/transform/08c7e932-41a2-460e-b14b-04903ac5ca8c/1442774135?io=transform:fill,width:792,height:900&quot;);"></div></div><div class="player-badge-card_playerInfo__3Fy6Q ff-mt-8 ff-mt-lg-20"><div class="player-badge-card_playerFirstname__1HlN6"><span class="d-md-none"><span title="Geronimo" style="display: -webkit-box; -webkit-line-clamp: 1; -webkit-box-orient: vertical; overflow: hidden; word-break: break-all;">Geronimo</span></span><span class="d-none d-md-block"><span title="Geronimo" style="display: -webkit-box; -webkit-line-clamp: 1; -webkit-box-orient: vertical; overflow: hidden; word-break: break-all;">Geronimo</span></span></div><div class="player-badge-card_playerSurname__3_5gf ff-mt-4"><span class="d-md-none"><span title="RULLI" style="display: -webkit-box; -webkit-line-clamp: 1; -webkit-box-orient: vertical; overflow: hidden; word-break: break-all;">RULLI</span></span><span class="d-none d-md-block"><span title="RULLI" style="display: -webkit-box; -webkit-line-clamp: 1; -webkit-box-orient: vertical; overflow: hidden; word-break: break-all;">RULLI</span></span></div></div><div class="player-badge-card_divider__kBsTp ff-mb-8 ff-mb-lg-16 ff-mt-4 ff-mt-lg-8"></div><div class="player-badge-card_playerPosition__wjnoI"><span class="d-md-none"><span title="Arquero" style="display: -webkit-box; -webkit-line-clamp: 1; -webkit-box-orient: vertical; overflow: hidden; word-break: break-all;">Arquero</span></span><span class="d-none d-md-block"><span title="Arquero" style="display: -webkit-box; -webkit-line-clamp: 1; -webkit-box-orient: vertical; overflow: hidden; word-break: break-all;">Arquero</span></span></div></div><div class="player-badge-card_badgeCard__2DJ4B"><div class="player-badge-card_playerFlag__rQjVe"><img decoding="async" loading="lazy" height="auto" width="100%" src="https://cloudinary.fifa.com/api/v3/picture/flags-sq-4/ARG?tx=c_fill,g_auto,q_auto" alt="" title="" class="image_img__3ckHZ player-badge-card_flag__2OQ8n"></div><div class="player-badge-card_playerImageContainer__1k0AM"><div class="player-badge-card_playerImage__301X0" style="background-image: url(&quot;https://digitalhub.fifa.com/transform/ec57583f-85ed-4b6e-b619-8ec94de13b8f/1442740128?io=transform:fill,width:792,height:900&quot;);"></div></div><div class="player-badge-card_playerInfo__3Fy6Q ff-mt-8 ff-mt-lg-20"><div class="player-badge-card_playerFirstname__1HlN6"><span class="d-md-none"><span title="Damian" style="display: -webkit-box; -webkit-line-clamp: 1; -webkit-box-orient: vertical; overflow: hidden; word-break: break-all;">Damian</span></span><span class="d-none d-md-block"><span title="Damian" style="display: -webkit-box; -webkit-line-clamp: 1; -webkit-box-orient: vertical; overflow: hidden; word-break: break-all;">Damian</span></span></div><div class="player-badge-card_playerSurname__3_5gf ff-mt-4"><span class="d-md-none"><span title="MARTINEZ" style="display: -webkit-box; -webkit-line-clamp: 1; -webkit-box-orient: vertical; overflow: hidden; word-break: break-all;">MARTINEZ</span></span><span class="d-none d-md-block"><span title="MARTINEZ" style="display: -webkit-box; -webkit-line-clamp: 1; -webkit-box-orient: vertical; overflow: hidden; word-break: break-all;">MARTINEZ</span></span></div></div><div class="player-badge-card_divider__kBsTp ff-mb-8 ff-mb-lg-16 ff-mt-4 ff-mt-lg-8"></div><div class="player-badge-card_playerPosition__wjnoI"><span class="d-md-none"><span title="Arquero" style="display: -webkit-box; -webkit-line-clamp: 1; -webkit-box-orient: vertical; overflow: hidden; word-break: break-all;">Arquero</span></span><span class="d-none d-md-block"><span title="Arquero" style="display: -webkit-box; -webkit-line-clamp: 1; -webkit-box-orient: vertical; overflow: hidden; word-break: break-all;">Arquero</span></span></div></div></div></div>';
+
+entry = prompt("Inserte etiquetas");
+
+let index = 0;
+let names = entry.split("</span></span>").map((cad) => {
+  index = cad.lastIndexOf(">");
+  return cad.slice(index + 1, cad.length);
+});
+
+const texto = entry,
+  regex =
+    /(?=(https:\/\/digitalhub.fifa.com\/transform\/[\dA-Za-z]{8}-[\dA-Za-z]{4}-[\dA-Za-z]{4}-[\dA-Za-z]{4}-[\dA-Za-z]{12}\/[\dA-Za-z]{10}))/g;
+let match;
+let images = [];
+
+while ((match = regex.exec(texto)) !== null) {
+  if (match.index === regex.lastIndex) {
+    regex.lastIndex++;
+  }
+  images.push(match[1]);
+}
+names = [...new Set([...names])].filter(
+  (cad) =>
+    ![
+      "Arquero",
+      "Defensor",
+      "Mediocampista",
+      "Delantero",
+      "Entrenador",
+      "",
+    ].includes(cad)
+);
+
+let fin = images.map((img, i) => {
+  return {
+    img,
+    name: names[i * 2],
+    last: names[i * 2 + 1],
+  };
+});
+
+console.log(fin);
